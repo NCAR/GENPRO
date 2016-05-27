11dfe9fb4207c298181ff09d54ff8b1d69e32c4b
Nicholas DeCicco <decicco@ucar.edu>
2016-05-27 14:22:33 -0600

    Remove redundant code.
    Simplify the implementation of convert.cpp by using gbytes()
    instead of my own solution, and delete convert2.cpp (which
    contains nothing of use).

--- g1nc/c/convert.cpp
+++ g1nc/c/convert.cpp
@@ -53,7 +53,6 @@ int main(int argc, char **argv)
 	int cmode = NC_NOCLOBBER;
 	FILE *fp;
 	char *inFileName, *outFileName;
-	uint32_t buf[3];
 	uint8_t header[HEADER_SIZE];
 	int data_decomp[HEADER_SIZE*8/20];
 	char header_decomp[HEADER_SIZE*8/6];
@@ -90,37 +89,7 @@ int main(int argc, char **argv)
 	}
 
 	// Extract 6 bit fields from this
-	for (j = 0, i = 0; i < HEADER_SIZE; i += 12 /* 3*32/8 */) {
-		buf[0] = (((uint32_t) header[i+ 0]) << 24) |
-		         (((uint32_t) header[i+ 1]) << 16) |
-		         (((uint32_t) header[i+ 2]) <<  8) |
-		         (((uint32_t) header[i+ 3]) <<  0);
-		buf[1] = (((uint32_t) header[i+ 4]) << 24) |
-		         (((uint32_t) header[i+ 5]) << 16) |
-		         (((uint32_t) header[i+ 6]) <<  8) |
-		         (((uint32_t) header[i+ 7]) <<  0);
-		buf[2] = (((uint32_t) header[i+ 8]) << 24) |
-		         (((uint32_t) header[i+ 9]) << 16) |
-		         (((uint32_t) header[i+10]) <<  8) |
-		         (((uint32_t) header[i+11]) <<  0);
-		header_decomp[j+15] = buf[2] & 0x3F; buf[2] >>= 6;
-		header_decomp[j+14] = buf[2] & 0x3F; buf[2] >>= 6;
-		header_decomp[j+13] = buf[2] & 0x3F; buf[2] >>= 6;
-		header_decomp[j+12] = buf[2] & 0x3F; buf[2] >>= 6;
-		header_decomp[j+11] = buf[2] & 0x3F; buf[2] >>= 6;
-		header_decomp[j+10] = (buf[2] & 0x3) | ((buf[1] & 0xF) << 2); buf[1] >>= 4;
-		header_decomp[j+9] = buf[1] & 0x3F; buf[1] >>= 6;
-		header_decomp[j+8] = buf[1] & 0x3F; buf[1] >>= 6;
-		header_decomp[j+7] = buf[1] & 0x3F; buf[1] >>= 6;
-		header_decomp[j+6] = buf[1] & 0x3F; buf[1] >>= 6;
-		header_decomp[j+5] = (buf[1] & 0xF) | ((buf[0] & 0x3) << 4); buf[0] >>= 2;
-		header_decomp[j+4] = buf[0] & 0x3F; buf[0] >>= 6;
-		header_decomp[j+3] = buf[0] & 0x3F; buf[0] >>= 6;
-		header_decomp[j+2] = buf[0] & 0x3F; buf[0] >>= 6;
-		header_decomp[j+1] = buf[0] & 0x3F; buf[0] >>= 6;
-		header_decomp[j+0] = buf[0] & 0x3F; buf[0] >>= 6;
-		j += 16;
-	}
+	gbytes<uint8_t,char>(header, header_decomp, 0, 6, 0, DECOMP_SIZE);
 
 	for (i = 0; i < DECOMP_SIZE; i++) {
 		header_decomp[i] = ascii[(int) header_decomp[i]];
