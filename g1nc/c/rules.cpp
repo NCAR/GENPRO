NCAR/GENPRO

commit dc4b72189a2a2d851b314fee323e481e1630cf5d

Don't convert all variables to float
...instead, let the rules decide what format to use for each variable,
then defer scaling/biasing until writing to NetCDF

g1nc/c/rules.cpp
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <regex.h>
+#include <netcdf.h>
 #include "gpfile.hpp"
 #include "rules.hpp"
 
@@ -38,7 +39,8 @@ RuleApplicatorData changeTimeRuleApplicators[] = {
 	{ rule_addAttr, &strptime_format },
 	{ rule_setDesc, (char*) "time of measurement" },
 	{ rule_addAttr, &time_standard_name },
-	{ rule_setTimeUnits, NULL }
+	{ rule_setTimeUnits, NULL },
+	{ rule_setPreferredType, (void*) NC_INT }
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -127,7 +129,8 @@ ParamRegexChangeRule actualRangeRule = {
 };
 
 RuleApplicatorData actualRangeRuleApplicators[] = {
-	{ rule_addMinMaxAttr, (char*) "actual_range" }
+	{ rule_addMinMaxAttr, (char*) "actual_range" },
+	{ rule_setPreferredType, (void*) NC_FLOAT }
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -228,7 +231,7 @@ Rule rules[] = {
 		&changeTimeRule,
 		rule_paramRegexChange,
 		changeTimeRuleApplicators,
-		/* .numApplicators = */ 5
+		/* .numApplicators = */ 6
 	},
 	// Change "ALAT" ("RAW INS LATITUDE") into "LAT"
 	{
@@ -246,7 +249,7 @@ Rule rules[] = {
 	{
 		&actualRangeRule,
 		rule_paramRegexChange,
-		actualRangeRuleApplicators, 1
+		actualRangeRuleApplicators, 2
 	}
 };
 
@@ -392,6 +395,18 @@ int rule_addAttr(void *applicatorData, void *extData, GP1File *const gp)
 	}
 
 	memcpy(param->attrs+(param->numAttrs-1), attr, sizeof(Attribute));
+
+	return 1;
+}
+
+int rule_setPreferredType(void *applicatorData, void *extData, GP1File *const gp)
+{
+	// g++ won't let us cast from void* to int in one go, so we have to resort
+	// to this nonsense
+	int preferredType = (int) ((long) applicatorData);
+	Parameter *const param = (Parameter*) extData;
+
+	param->preferredType = preferredType;
 
 	return 1;
 }

g1nc/c/genpro2nc.cpp
@@ -28,6 +28,8 @@
 #define DECOMP_HEADER_SIZE GET_DECOMP_HEADER_SIZE(HEADER_LINES)
 #define PARAMETER_HEADER_START COMP_HEADER_SIZE
 
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
 enum {
 	kFalse = 0,
 	kTrue = 1
@@ -219,7 +221,7 @@ int gp1_read(GP1File *const gp, FILE *fp)
 		if (gp->params[i].isUnused) continue;
 		gp->params[i].numValues = gp->params[i].rate * gp->numBlocks * gp->cyclesPerBlock;
 		if (!(gp->params[i].values =
-		      (float*) malloc(sizeof(float)*gp->params[i].numValues)))
+		      (int*) malloc(sizeof(int)*gp->params[i].numValues)))
 		{
 			goto mallocfail;
 		}
@@ -256,16 +258,6 @@ int gp1_read(GP1File *const gp, FILE *fp)
 
 	free(in_buffer); in_buffer = NULL;
 	free(data_decomp); data_decomp = NULL;
-
-	// Restore original values by scaling/biasing.
-	for (i = 0; i < gp->numParameters; i++) {
-		if (gp->params[i].isUnused) continue;
-		for (j = 0; j < (int) gp->params[i].numValues; j++) {
-			gp->params[i].values[j] = ((float) gp->params[i].values[j])/
-			                          gp->params[i].scale -
-			                          gp->params[i].bias;
-		}
-	}
 
 	return 1;
 
@@ -347,7 +339,7 @@ int gp1_write_nc(GP1File const*const gp,
 	int ncid; /* Handle on the NetCDF file */
 	char name[100];
 	int status;
-	int i;
+	int i, j;
 	int timeDimId;
 	int numUniqueRates;
 	int *uniqueRates;
@@ -357,6 +349,8 @@ int gp1_write_nc(GP1File const*const gp,
 	long numDims;
 	size_t start[2] = { 0, 0 };
 	size_t count[2];
+	void *values;
+	size_t maxNumValues;
 
 	if ((status = nc_create(outFileName, cmode, &ncid)) != NC_NOERR) {
 		fprintf(stderr, "Fatal: failed to create NetCDF file.\n");
@@ -404,8 +398,9 @@ int gp1_write_nc(GP1File const*const gp,
 			dimIds[1] = rateDimIds[lookupTable[i]];
 		}
 
-		if ((status = nc_def_var(ncid, gp->params[i].label, NC_FLOAT, numDims,
-		                         dimIds,
+		if ((status = nc_def_var(ncid, gp->params[i].label,
+		                         gp->params[i].preferredType,
+		                         numDims, dimIds,
 		                         &(gp->params[i].ncVar))) != NC_NOERR)
 		{
 			fprintf(stderr, "NetCDF variable definition failed, aborting.\n");
@@ -440,9 +435,7 @@ int gp1_write_nc(GP1File const*const gp,
 	// Add global attributes which may have been added by the rule set.
 	gp1_addAttrs(ncid, NC_GLOBAL, gp->attrs, gp->numAttrs);
 
-	free(uniqueRates);
 	free(lookupTable);
-
 
 	if ((status = nc_put_att_text(ncid, NC_GLOBAL, "DESCRIPTION",
 	                              gp->fileDescLen, gp->fileDesc)) != NC_NOERR)
@@ -458,16 +451,51 @@ int gp1_write_nc(GP1File const*const gp,
 
 	nc_enddef(ncid);
 
+
+	// Restore original values by scaling/biasing.
+
+	// Allocate an array big enough to hold the largest array
+	maxNumValues = uniqueRates[numUniqueRates-1]*gp->numBlocks*gp->cyclesPerBlock;
+	if (!(values = malloc(max(sizeof(float),sizeof(int))*maxNumValues))) {
+		return 0;
+	}
+
 	count[0] = gp->numBlocks * gp->cyclesPerBlock;
 	for (i = 0; i < gp->numParameters; i++) {
 		if (gp->params[i].isUnused) continue;
 		count[1] = gp->params[i].rate;
-		if (nc_put_vara_float(ncid, gp->params[i].ncVar, start, count,
-		                     gp->params[i].values) != NC_NOERR)
-		{
-			goto ncerr;
+
+		switch (gp->params[i].preferredType) {
+			case NC_FLOAT:
+				for (j = 0; j < (int) gp->params[i].numValues; j++) {
+					((float*) values)[j] = ((float) gp->params[i].values[j])/
+					                       gp->params[i].scale -
+					                       gp->params[i].bias;
+				}
+				if (nc_put_vara_float(ncid, gp->params[i].ncVar, start, count,
+				                      (float*) values) != NC_NOERR)
+				{
+					goto ncerr;
+				}
+				break;
+
+			case NC_INT:
+				for (j = 0; j < (int) gp->params[i].numValues; j++) {
+					((int*) values)[j] = gp->params[i].values[j]/
+					                     gp->params[i].scale -
+					                     gp->params[i].bias;
+				}
+				if (nc_put_vara_int(ncid, gp->params[i].ncVar, start, count,
+				                    (int*) values) != NC_NOERR)
+				{
+					goto ncerr;
+				}
+				break;
 		}
 	}
+
+	free(values);
+	free(uniqueRates);
 
 	nc_close(ncid);
 

g1nc/c/gpfile.hpp
@@ -30,8 +30,9 @@ typedef struct {
 	                      terminator. */
 	int ncVar;        /** The NetCDF variable ID corresponding to this
 	                      parameter. */
-	float *values;    /** A pointer to an array of values for this
-	                      parameter. */
+	int preferredType; /** Preferred output type. */
+	int *values;      /** A pointer to an array of raw (i.e., unscaled and
+	                      unbiased) values for this parameter. */
 	size_t numValues; /** Total number of samples recorded. This is the length
 	                      of the `values' array. */
 	char isUnused;    /** Indicates if the variable is unused. */

g1nc/c/rules.hpp
@@ -50,6 +50,7 @@ int rule_addGlobalAttr(void *applicatorData, void *extData, GP1File *const gp);
 int rule_addGlobalMinMax(void *applicatorData, void *extData, GP1File *const gp);
 int rule_addMinMaxAttr(void *applicatorData, void *extData, GP1File *const gp);
 int rule_setTimeUnits(void *applicatorData, void *extData, GP1File *const gp);
+int rule_setPreferredType(void *applicatorData, void *extData, GP1File *const gp);
 
 //
 // Rules

commit b10a87e3cc8d9bb9235ee3972cbf65a1b947d6a7

Fix a regression introduced by dc4b7218
Scale and bias min/max values generated in the rule set to account for
the fact that parameter values are now stored as integers.

g1nc/c/rules.cpp
@@ -268,20 +268,24 @@ int rule_addMinMaxAttr(void *applicatorData, void *extData, GP1File *const gp)
 	Parameter *const param = (Parameter*) extData;
 	char *const name = (char*) applicatorData;
 	float *minmax;
+	int minmax_i[2];
 	size_t i;
 
 	if (!(minmax = (float*) malloc(sizeof(float)*2))) return 0;
 
 	// Compute min/max values
-	minmax[0] = minmax[1] = param->values[0];
+	minmax_i[0] = minmax_i[1] = param->values[0];
 	for (i = 0; i < param->numValues; i++) {
-		if (minmax[0] > param->values[i]) {
-			minmax[0] = param->values[i];
+		if (minmax_i[0] > param->values[i]) {
+			minmax_i[0] = param->values[i];
 		}
-		if (minmax[0] < param->values[i]) {
-			minmax[0] = param->values[i];
+		if (minmax_i[0] < param->values[i]) {
+			minmax_i[0] = param->values[i];
 		}
 	}
+
+	minmax[0] = ((float) minmax_i[0])/param->scale - param->bias;
+	minmax[1] = ((float) minmax_i[1])/param->scale - param->bias;
 
 	Attribute attr = { name, kAttrTypeFloat, minmax, 2 };
 
@@ -297,6 +301,7 @@ int rule_addGlobalMinMax(void *applicatorData, void *extData, GP1File *const gp)
 	char *const formatStr = (char*) applicatorData;
 	char *minName, *maxName;
 	float *min, *max;
+	int min_i, max_i;
 	size_t i;
 
 	if (!(minName = (char*) malloc(sizeof(char)*BUF_SIZE))) return 0;
@@ -309,15 +314,18 @@ int rule_addGlobalMinMax(void *applicatorData, void *extData, GP1File *const gp)
 	snprintf(maxName, BUF_SIZE, formatStr, "max");
 
 	// Compute min/max values
-	*min = *max = param->values[0];
+	min_i = max_i = param->values[0];
 	for (i = 0; i < param->numValues; i++) {
-		if (*min > param->values[i]) {
-			*min = param->values[i];
+		if (min_i > param->values[i]) {
+			min_i = param->values[i];
 		}
-		if (*max < param->values[i]) {
-			*max = param->values[i];
+		if (max_i < param->values[i]) {
+			max_i = param->values[i];
 		}
 	}
+
+	*min = ((float) min_i)/param->scale - param->bias;
+	*max = ((float) max_i)/param->scale - param->bias;
 
 	Attribute minAttr = { minName, kAttrTypeFloat, min, 1 };
 	Attribute maxAttr = { maxName, kAttrTypeFloat, max, 1 };