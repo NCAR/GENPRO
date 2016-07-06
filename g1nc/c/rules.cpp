52ef81f2a69f053b33dbba3c1d93f0125c617c84
Nicholas DeCicco <decicco@ucar.edu>
2016-07-06 10:40:39 -0600

    Fix for N-UNITS
    Refractive indices are listed as having units of 'N'. Change this to
    'none', but only if the description contains the string 'REFRACTIVE
    INDEX'.

--- g1nc/c/rules.cpp
+++ g1nc/c/rules.cpp
@@ -405,8 +405,9 @@ int rule_makeUnitsCFCompliant(void *applicatorData,
 	/* degrees Celsius */
 	else if (UNITS_EQUALS("C"))        SET_UNITS("deg_C");
 
-	/* degrees Celsius */
-	else if (UNITS_EQUALS("N-UNITS"))  SET_UNITS("none");
+	/* refractive index */
+	else if (strncmp(param->desc, "REFRACTIVE INDEX", param->descLen) &&
+	         UNITS_EQUALS("N"))        SET_UNITS("none");
 
 	/* unknown */
 	else if (UNITS_EQUALS(""))         SET_UNITS("unk");
